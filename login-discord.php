1c93a0e10761589d3033d7dfa2574285faf8e805
Charles Montalvo <bonelesschachii@Charless-MacBook-Air.local>
2025-04-11 14:02:03 -0400

    URL change

--- login-discord.php
+++ login-discord.php
@@ -1,7 +1,7 @@
 <?php
 // Discord OAuth2 login redirect
 $client_id = '1357254475080794184'; // client ID
-$redirect_uri = urlencode('http://localhost/LoveConnect/discord-callback.php');
+$redirect_uri = urlencode('http://localhost/CO-OP/discord-callback.php');
 $scope = 'identify email';
 $response_type = 'code';
 
